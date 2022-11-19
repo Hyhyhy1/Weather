a35bc7131e625dc55b3f5ab3d659bcbba6ac5133
Hyhyhy1 <kuleshov.nikita@urfu.me>
2022-11-20 01:57:06 +0500

    добавлены виджеты
    добавлена возможность создавать множественные виджеты

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
+let widgetsCounter = 0;
+let latitude = 0
+let longitude = 0;
+
 document.forms.publish.onsubmit = function() {
-    let latitude = this.latitude.value;
-    let longitude = this.longitude.value;
+    widgetsCounter += 1;
+    latitude = this.latitude.value;
+    longitude = this.longitude.value;
     const url = 'https://api.open-meteo.com/v1/forecast?latitude=' + String(latitude) +
-            '&longitude=' + String(longitude) + '&hourly=temperature_2m,weathercode'
-   sendRequest(url)
-        .then(data => printResponce(data))
+            '&longitude=' + String(longitude) + '&hourly=temperature_2m,weathercode,windspeed_10m'
+    sendRequest(url).then(data => formWidget(data));
     return false
 };
 
@@ -13,54 +17,107 @@ function sendRequest(url){
         return responce.json()
     })
 }
-
-function printResponce(responce){
-    date = getCurrentDate();
+//функция получилось большой, но есть ли смысл её дробить на более мелкие? Она создает html разметку виджетов-элементов списка
+//можно было отделить создание внутренних элементов от оберток, но есть ли в этом смысл?
+function formWidget(responce){
+    let widgetsContainer = document.getElementById("widgets_container");
     console.log(responce);
 
+    let listElement = document.createElement('li');
+    listElement.className += "widgets_list_item";
+
+    let listElementWrapper = document.createElement('div');
+    listElementWrapper.className += "widgets_wrapper";
+
+    let widgetHeader = document.createElement('div');
+    widgetHeader.className += "widgets_header"
+
+    let headerTemperature = document.createElement('div');
+    headerTemperature.className += "widgets_header_temperature";
+    
+    let temperature = document.createElement('p');
+    temperature.className += "widgets_temperature";
+
+    let temperatureIcon = document.createElement('img');
+    temperatureIcon.className += "widgets_icon";
+
+    let weathercode = document.createElement('p');
+    weathercode.className += "widgets_weathercode";
+
+    let windSpeed = document.createElement('p');
+    windSpeed.className += "widgets_windSpeed";
+    
+    let map = document.createElement('div');
+    map.className += "widgets_map";
+    map.id = String(widgetsCounter);
+    ymaps.ready(initMap);
+
+    headerTemperature.appendChild(temperature);
+    headerTemperature.appendChild(temperatureIcon);
+    widgetHeader.appendChild(headerTemperature);
+    widgetHeader.appendChild(weathercode);
+    widgetHeader.appendChild(windSpeed);
+    listElementWrapper.appendChild(widgetHeader);
+    listElementWrapper.appendChild(map);
+    listElement.appendChild(listElementWrapper);
+    widgetsContainer.appendChild(listElement);
+
+    fillWeatherData(temperature, temperatureIcon, weathercode, windSpeed, responce)
+}
+
+function initMap(){
+    let map = new ymaps.Map(String(widgetsCounter),{
+        center: [latitude, longitude],
+        zoom: 7
+    })
+}
+
+function getCurrentDateIndex(responce){
+    //получение текущей даты в соответствующем запросу форматте
+    date = new Date();
+    date = new Date(date.setMilliseconds(1 * 60 * 60 * 1000));
+    date = date.toISOString().slice(0,14) + '00';
+
+    //получение индекса, которому соответствует текущая дата в массиве дат ответа api 
     let index = 0;
     while (responce.hourly.time[index] != date){
         index+=1;
     }
-
-    document.getElementById("temperature").innerHTML = responce.hourly.temperature_2m[index] + ' ' + responce.hourly_units.temperature_2m;
-    document.getElementById("weathercode").innerHTML = getWeather(responce.hourly.weathercode[index])
-    //document.getElementById("image").src = 104f876b-9e10-4987-a817-677a0db09e2b   
+    return index;
 }
 
-function getCurrentDate(){
-    date = new Date();
-    date = new Date(date.setMilliseconds(1 * 60 * 60 * 1000));
-    date = date.toISOString().slice(0,14) + '00';
-    return date;
-}
+function fillWeatherData(temperature, temperatureIcon, weathercode, windSpeed, responce){ 
+    let index = getCurrentDateIndex(responce);
+    let code = responce.hourly.weathercode[index];
+    temperature.textContent = responce.hourly.temperature_2m[index] + ' ' + responce.hourly_units.temperature_2m;
+    windSpeed.textContent = 'Скорость ветра ' + responce.hourly.windspeed_10m[index] + ' км/ч';
 
-function getWeather(weathercode){ 
-    if (weathercode == 0){
-        return 'Ясно';
+    if (code == 0){
+        weathercode.textContent = 'Ясно';
+        temperatureIcon.src = "images/day.svg";
 
-    } else if(weathercode > 0 && weathercode <=3){
-        return 'Частично облачно';
+    } else if(code > 0 && code <=3){
+        weathercode.textContent = 'Частично облачно';
+        temperatureIcon.src = "images/cloudy.svg";
 
-    } else if(weathercode > 44 && weathercode <=48){
-        return 'Туман';
+    } else if(code > 44 && code <=48){
+        weathercode.textContent = 'Туман';
+        temperatureIcon.src = "images/cloudy.svg";
 
-    }else if(weathercode > 50 && weathercode <=57){
-        return 'Морось';
+    } else if((code > 60 && code <=67) || (code > 79 && code <=85) || (code > 50 && code <=57)){
+        weathercode.textContent = 'Дождь';
+        temperatureIcon.src = "images/rainy.svg";
 
-    } else if(weathercode > 60 && weathercode <=67){
-        return 'Дождь';
+    } else if(code > 70 && code <=77){
+        weathercode.textContent = 'Снегопад';
+        temperatureIcon.src = "images/snowy.svg";
 
-    } else if(weathercode > 70 && weathercode <=77){
-        return 'Снегопад';
+    }else if(code >94 && code <=99){
+        weathercode.textContent = 'Гроза';
+        temperatureIcon.src = "images/thunder.svg";
 
-    }else if (weathercode > 79 && weathercode <=82){
-        return 'Небольшой ливневый дождь';
-
-    }else if(weathercode >94 && weathercode <=99){
-        return 'Гроза';
-
-    } else{
-        return 'Неизвестный код погоды';
+    }else{
+        weathercode.textContent = 'Неизвестный код погоды';
+        temperatureIcon.src = "images/not-applicable.png";
     }
 }
